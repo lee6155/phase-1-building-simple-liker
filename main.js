14021e124780d500c62ac15ef3ae81f22a13af89
Evan Lee <evan.lee.615@gmail.com>
2024-03-17 20:35:02 -0400

    complete lab

--- main.js
+++ main.js
@@ -4,7 +4,27 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+let hearts = document.getElementsByClassName('like-glyph')
+heartArray = [...hearts]
+heartArray.forEach(function(item){
+  item.addEventListener('click', tryFunction)
+  })
+  
+function tryFunction (e) {
+    let heart = document.getElementById(e.target.id)
+    mimicServerCall()
+    .then(heart.textContent = '♥')
+    .then(heart.style.color = "red")
+    .then(heart.addEventListener('click', function(){
+      heart.textContent = '♡'
+      heart.style.color = "rgb(170,184,194)"
+    }))
+    .catch(function(){
+      document.getElementById('modal').hidden = false
+      document.getElementById('modal-message').textContent = 'Random server error. Try again.'
+      setTimeout(() => document.getElementById('modal').hidden = true, 3000)
+      })
+    }
 
 
 //------------------------------------------------------------------------------
